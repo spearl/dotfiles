5bdfda7365e8963c06c476c73472bb1c01c68ce0
Landon Spear <phyujin@gmail.com>
2022-04-08 10:18:35 -0700

    vscode extension script

---
+++ vscode.sh
@@ -0,0 +1,42 @@
+code --install-extension ms-python.python
+
+code --install-extension mgmcdermott.vscode-language-babel
+
+code --install-extension bungcip.better-toml
+
+code --install-extension ms-azuretools.vscode-docker
+
+code --install-extension pkief.material-icon-theme
+
+code --install-extension akamud.vscode-theme-onelight
+
+code --install-extension akamud.vscode-theme-onedark
+
+code --install-extension dbaeumer.vscode-eslint
+
+code --install-extension jmsv.javascriptsnippetsstandard
+
+code --install-extension redhat.vscode-yaml
+
+code --install-extension ms-vscode.sublime-keybindings
+
+code --install-extension msjsdiag.vscode-react-native
+
+code --install-extension eamodio.gitlens
+
+code --install-extension golang.go
+
+code --install-extension graphql.vscode-graphql
+
+code --install-extension arcticicestudio.nord-visual-studio-code
+
+code --install-extension esbenp.prettier-vscode
+
+code --install-extension rebornix.ruby
+
+code --install-extension castwide.solargraph
+
+code --install-extension kaiwood.endwise
+
+code --install-extension misogi.ruby-rubocop
+
